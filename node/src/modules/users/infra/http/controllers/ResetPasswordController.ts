b752f2bc583cbe25f012c8efd799c93c18805bd8
Luísa Braga <mlpb@icomp.ufam.edu.br>
2020-07-22 00:00:39 -0400

    feat: users tokens

---
+++ node/src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const resetPasswordService = container.resolve(ResetPasswordService);
+    const { password, token } = req.body;
+    await resetPasswordService.execute({
+      token,
+      password,
+    });
+    return res.status(204).json();
+  }
+}
---
+++ node/src/modules/users/infra/http/routes/password.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
+
+const passwordRouter = Router();
+const forgotController = new ForgotPasswordController();
+const resetController = new ResetPasswordController();
+
+passwordRouter.post('/forgot', forgotController.create);
+passwordRouter.post('/reset', resetController.create);
+
+export default passwordRouter;
